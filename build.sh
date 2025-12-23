2b4fbd909194170f185f6d32d743285f4d2976c4
Osama Abdullah <abdullah21673@hotmail.com>
2025-12-23 15:03:51 +0600

    remove vercel computer update command

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 pip install -r requirements.txt
-apt-get update && apt-get install -y build-essential
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
