42103e4c14face9fff47de1555ec7f75127037b9
Osama Abdullah <abdullah21673@hotmail.com>
2025-12-23 20:48:13 +0600

    render allowed host added

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").strip().lower() in ["true", "1", "yes"]
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', '.onrender.com']
 
 INTERNAL_IPS = [
     # ...
