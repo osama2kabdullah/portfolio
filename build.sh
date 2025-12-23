25d7c3191b27d8cfb238b29dc0dca4326c4f64aa
Osama Abdullah <abdullah21673@hotmail.com>
2025-12-23 15:02:41 +0600

    config setup for vercel

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status
 set -e
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Apply migrations
+apt-get update && apt-get install -y build-essential
 python manage.py migrate --noinput
-
-# Collect static files
 python manage.py collectstatic --noinput
